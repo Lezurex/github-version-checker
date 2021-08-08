9ae43c3948efeed5c1c2ce1116780fa540362720
Lenny Angst <lenny.angst@easyid.ch>
2021-08-08 03:22:40 +0200

    Add repo existing check Add gson dependency

--- src/main/java/com/lezurex/githubversionchecker/GithubVersionChecker.java
+++ src/main/java/com/lezurex/githubversionchecker/GithubVersionChecker.java
@@ -1,5 +1,12 @@
 package com.lezurex.githubversionchecker;
 
+import com.lezurex.githubversionchecker.exceptions.RepoNotFoundException;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class GithubVersionChecker {
 
     /**
@@ -10,13 +17,27 @@ public class GithubVersionChecker {
      * Name of the repository on GitHub
      */
     private final String repo;
+    private final ReleaseVersion currentVersion;
 
     /**
      * @param username Username where the targeted repo is located
      * @param repo Name of the repository on GitHub
+     * @param currentVersion The current version running
      */
-    public GithubVersionChecker(String username, String repo) {
+    public GithubVersionChecker(String username, String repo, ReleaseVersion currentVersion) {
         this.username = username;
         this.repo = repo;
+        this.currentVersion = currentVersion;
+
+        try {
+            URL url = new URL(String.format("https://api.github.com/repos/%s/%s", this.username, this.repo));
+            HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            if (con.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                throw new RepoNotFoundException(this.username, this.repo);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
