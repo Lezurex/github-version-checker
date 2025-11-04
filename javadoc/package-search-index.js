4c99c1a2043c6c076567f53bca7e7b173fff4d7d
Lezurex <Lezurex@users.noreply.github.com>
2025-11-04 15:40:03 +0000

    Deploying to javadoc from @ Lezurex/github-version-checker@e6d1277d27063a7bc447eeef689641d9317e4e87 🚀

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.lezurex.githubversionchecker"},{"l":"com.lezurex.githubversionchecker.exceptions"}];updateSearchResults();
